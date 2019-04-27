e35935cb5f35ab391d319e803c024bc516cf2b79
Thomas Seng Hin Mak <mak@makzan.net>
2019-04-27 10:39:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,30 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+    body {
+        margin: 0;
+        padding: 1em;
+    }
+    input {
+        width: 100%;
+        padding: .5em;
+    }
+    input[name='password'] {
+        color: #efefef;
+    }
+    textarea {
+        width: 100%;
+        font-size: 1.5em;
+        color: grey;
+    }
+</style>
 <form action='' method='post'>
-    <input name='password' value=<?=$_POST['password']??""?>>
-    <input type='submit'>
+    <input autofocus name='password' value=<?=$_POST['password']??""?>>
+    <input type='submit' value='Generate'>
 </form>
 <textarea><?php
-  echo(hash('md5', $_POST['password']??""));
+  echo(hash('sha256', $_POST['password']??""));
 ?>
 </textarea>
+<p>
+  <small>SHA256 Generator</small>
+</p>
